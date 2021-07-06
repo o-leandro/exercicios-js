8346babd3f0efb8fd3aeb2e46973134ea4421575
Leandro Santos <leanddrosanttoss@gmail.com>
2021-07-06 10:57:27 -0300

    Desafio função construtora

---
+++ controle/desafioFuncaoConstrutora.js
@@ -0,0 +1,28 @@
+function Pessoa(nome) {
+    this.nome = nome
+
+    this.falar = function() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('Mariazinha')
+p1.falar()
+ 
+ 
+ 
+ /* class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('Maria')
+p1.falar()
+*/
+
+
+

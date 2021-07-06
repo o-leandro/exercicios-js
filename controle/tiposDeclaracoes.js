ae374525efa170ea494257dabd698c724cc6aadb
Leandro Santos <leanddrosanttoss@gmail.com>
2021-07-05 22:45:40 -0300

    Tipos de declarações de funções

---
+++ controle/tiposDeclaracoes.js
@@ -0,0 +1,19 @@
+console.log(soma(2, 3))
+
+// function declaration
+function soma (x, y) {
+    return x + y
+}
+
+// function expression
+const sub = function(x, y) {
+    return x - y
+}
+console.log(sub(5, 6))
+
+
+// named function expression
+const mult = function mult (x, y) {
+    return x * y
+}
+console.log(mult(3, 3))

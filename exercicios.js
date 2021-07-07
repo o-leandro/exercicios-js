d5200f7123c079beb37cde05bcdfca7de853219e
Leandro Santos <leanddrosanttoss@gmail.com>
2021-07-07 18:21:30 -0300

    Lista de exercícios

---
+++ exercicios.js
@@ -0,0 +1,12 @@
+// 01)
+function operacoes(n1 = 4, n2 = 4) {
+    console.log(`A soma de ${n1} + ${n2} é: `, n1 + n2)
+    console.log(`A subtração de ${n1} - ${n2} é: `, n1 - n2)
+    console.log(`A multiplicação de ${n1} * ${n2} é: `, n1 * n2)
+    console.log(`A divisão de ${n1} / ${n2} é: `, n1 / n2)
+}
+
+operacoes()
+
+
+
